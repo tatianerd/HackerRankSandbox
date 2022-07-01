dbd96c17e81197c9d6848a696da612cf1aa50f75
Tatiane Domingues <tatiane_@outlook.com>
2022-07-01 20:43:30 -0300

    Add project files.

---
+++ HackerRankSandbox/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+List<int> q = new List<int>() { 1, 2, 5, 3, 7, 8, 6, 4 };
+
+
+var s = 0;
+var j = 1;
+var count = q.Count;
+var r = q.OrderBy(x => x).ToList();
+
+for (var i = 0; i < count; i++)
+{
+    if (q[i] - (i + 1) > 2)
+    {
+        Console.WriteLine("Too chaotic");
+        break;
+    }
+
+    if (j < count && q[i] > q[j])
+    {
+        var p = r.FindIndex(x => x == q[i]);
+
+
+    }
+
+    j++;
+
+}
+
+if (s > 0)
+    Console.WriteLine(s);
